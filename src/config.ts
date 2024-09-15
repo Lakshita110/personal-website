26bab12793a6fb5f5e9f804906beacd1795c832c
Lakshita <lak.jai110@gmail.com>
2024-09-15 16:59:09 -0400

    Update homepage and navbar

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Lakshita Jain";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "I am Lakshita Jain! Welcome to my homepage.";
+export const MY_NAME = "Lakshita Jain";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
